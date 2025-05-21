d5252e8f5ae016067a98739f5ba4308e953d8c6a
Julius Vincent D. Llavan <202280209@psu.palawan.edu.ph>
2025-05-22 01:22:12 +0800

    Add initial models, views, and URLs for dashboards

--- apps/dashboards/urls.py
+++ apps/dashboards/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import DashboardsView
-
+from .views import PieCountbySeverity, LineCountByMonth, MultilineIncidentTop3Country, MultipleBarbySeverity
 
 
 urlpatterns = [
-    path(
-        "",
-        DashboardsView.as_view(template_name="dashboard_analytics.html"),
-        name="index",
-    )
+    path('', DashboardsView.as_view(template_name="dashboard_analytics.html"), name="index"),
+    path('dashboard/analytics/', DashboardsView.as_view(template_name="dashboard_analytics.html"), name='dashboard-chart'),
+    path('pieChart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountByMonth, name='chart'),
+    path('multiLineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', MultipleBarbySeverity, name='chart'),
 ]
